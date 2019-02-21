d1225c3ad5915077dbd0c831a4bb5a7981b46a5d
aleto44 <aleto@live.unc.edu>
2019-02-21 14:47:08 -0500

    picture comes up now

--- public/app.js
+++ public/app.js
@@ -1,27 +1,32 @@
-
-
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
     console.log(app)
 });
 
-function tempUserLogin(){
+function tempUserLogin() {
     var db = firebase.database();
     var userInput = document.getElementById('usernameBox').value;
 
-    db.ref('TempUser/').push(
-        {
-            Username: userInput
-        }
-    );
+    db.ref('TempUser/').push({
+        Username: userInput
+    });
     console.log("what");
 }
 
-// function readDB(){
-//     alert(getDB());
-// }
-// function getDB(){
-//     return firebase.database().ref('/users/gameOne').once('value').then(function(snapshot) {
-//         var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-//       });
-// }
+function readDB() {
+    var ref = firebase.database().ref("Images/keyGUID/");
+
+    ref.on("value", function (snapshot) {
+        console.log(snapshot.val());
+    }, function (error) {
+        console.log("Error: " + error.code);
+    });
+
+    console.log();
+    var url = "https://i.imgur.com/qSnnQyU.jpg";
+
+    var img = document.createElement("img");
+    img.src = url;
+    document.body.appendChild(img);
+
+}
