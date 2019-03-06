25daf2beaf3e4cc871a682ae2db2c74ad5491584
aleto44 <aleto@live.unc.edu>
2019-03-06 12:29:44 -0500

    gertandimint and read db put in

--- game/src/Test.js
+++ game/src/Test.js
@@ -2,28 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 import firebase from './firebase'
 
+
 class App extends Component {
 
-    
-
-    appendImage()
+    readDB()
     {
-        var ref = firebase.database().ref("images/"+ 1);
+        for(var i = 0; i < 4; i++)
+        {
+            var randomIndex = this.getRandomInt(0,833);
+            this.appendImage(randomIndex, i);
+        }
+    }
+
+    appendImage(index, currentCardNumber)
+    {
+        var ref = firebase.database().ref("images/"+ index);
         ref.on("value", function (snapshot) {
-            console.log(snapshot.val());
+            console.log(snapshot.val().url);
+            test = snapshot.val().url;
         }, function (error) {
             console.log("Error: " + error.code);
         });
+        console.log(test);
     }
 
+    getRandomInt(min, max) 
+    {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
   render() {
     return (
         <div>
 
-        <button onClick={this.appendImage}>
+        <button onClick={ () => this.readDB()}>
             Get Random Images
         </button>
+        <img src={this.test} />
         </div>
     );
   }
